fdd025da1662e64ecd02d0e895dae621b76173f7
Abdullah Farag <es-abdullahfaragg2025@alexu.edu.eg>
2024-10-21 16:15:58 +0400

    change the names and add feature to toggle years

--- src/LunarCalendar.js
+++ src/LunarCalendar.js
@@ -19,6 +19,12 @@ const LunisolarHijriCalendar = () => {
     'Last Quarter': '#FF9933'
   };
 
+  const hijriMonths = [
+    "Muharram", "Safar", "Rabi' al-Awwal", "Rabi' al-Thani",
+    "Jumada al-Ula", "Jumada al-Thani", "Rajab", "Sha'ban",
+    "Ramadan", "Shawwal", "Dhu al-Qi'dah", "Dhu al-Hijjah"
+  ];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -51,11 +57,12 @@ const LunisolarHijriCalendar = () => {
   }, []);
 
   const processLunisolarData = (data) => {
-    let hijriYear = 1;
+    let hijriYear = null;
     let hijriMonth = 1;
     let hijriDay = 1;
     let isOddMonth = true;
     const HIJRI_EPOCH = 622;
+    const HIJRI_EPOCH_DATE = moment('622-07-16', 'YYYY-MM-DD');
     const SOLAR_YEAR = 365.2422;
     const LUNAR_YEAR = 354.36707;
     const events = [];
@@ -66,49 +73,53 @@ const LunisolarHijriCalendar = () => {
     data.forEach((row, index) => {
       const gregorianDate = moment(row.datetime, 'YYYY-MM-DD HH:mm:ss');
       
-      // Set earliest date
       if (!earliestDate || gregorianDate.isBefore(earliestDate)) {
         earliestDate = gregorianDate.toDate();
       }
 
-      if (lastFullMoon) {
-        const daysSinceLastFullMoon = gregorianDate.diff(lastFullMoon, 'days');
-        hijriDay += daysSinceLastFullMoon;
-        
-        while (hijriDay > (isOddMonth ? 30 : 29)) {
-          hijriDay -= isOddMonth ? 30 : 29;
-          hijriMonth++;
-          isOddMonth = !isOddMonth;
+      if (gregorianDate.isSameOrAfter(HIJRI_EPOCH_DATE)) {
+        if (hijriYear === null) {
+          const yearsSinceEpoch = gregorianDate.diff(HIJRI_EPOCH_DATE, 'years', true);
+          hijriYear = Math.floor(yearsSinceEpoch * (SOLAR_YEAR / LUNAR_YEAR)) + 1;
+        }
 
-          if (hijriMonth > 12) {
-            hijriMonth = 1;
-            hijriYear++;
+        if (lastFullMoon) {
+          const daysSinceLastFullMoon = gregorianDate.diff(lastFullMoon, 'days');
+          hijriDay += daysSinceLastFullMoon;
+          
+          while (hijriDay > (isOddMonth ? 30 : 29)) {
+            hijriDay -= isOddMonth ? 30 : 29;
+            hijriMonth++;
+            isOddMonth = !isOddMonth;
 
-            accumulatedDrift += (SOLAR_YEAR - LUNAR_YEAR);
-            if (accumulatedDrift >= LUNAR_YEAR) {
-              hijriMonth++;
-              accumulatedDrift -= LUNAR_YEAR;
+            if (hijriMonth > 12) {
+              hijriMonth = 1;
+              hijriYear++;
+
+              accumulatedDrift += (SOLAR_YEAR - LUNAR_YEAR);
+              if (accumulatedDrift >= LUNAR_YEAR) {
+                hijriMonth++;
+                accumulatedDrift -= LUNAR_YEAR;
+              }
             }
           }
         }
-      }
-      
-      if (row.phase === 'Full Moon') {
-        lastFullMoon = gregorianDate;
-
-        const gregorianYear = gregorianDate.year();
-        if (gregorianYear >= HIJRI_EPOCH) {
-          const yearsSinceEpoch = gregorianYear - HIJRI_EPOCH;
-          hijriYear = Math.floor(yearsSinceEpoch * (SOLAR_YEAR / LUNAR_YEAR)) + 1;
+        
+        if (row.phase === 'Full Moon') {
+          lastFullMoon = gregorianDate;
         }
       }
 
+      const hijriDateString = hijriYear !== null 
+        ? `${hijriYear} ${hijriMonths[hijriMonth - 1]} ${hijriDay}` 
+        : 'Pre-Hijri';
+
       events.push({
-        title: `${row.phase} - Hijri: ${hijriYear}-${hijriMonth}-${hijriDay}`,
+        title: `${row.phase}${hijriYear !== null ? ` - Hijri: ${hijriDateString}` : ''}`,
         start: gregorianDate.toDate(),
         allDay: true,
         extendedProps: {
-          hijriDate: `${hijriYear}-${hijriMonth}-${hijriDay}`,
+          hijriDate: hijriDateString,
           gregorianDate: gregorianDate.format('YYYY-MM-DD'),
           phase: row.phase
         },
@@ -125,8 +136,12 @@ const LunisolarHijriCalendar = () => {
       <>
         <b>{eventInfo.event.extendedProps.phase}</b>
         <br />
-        Hijri: {eventInfo.event.extendedProps.hijriDate}
-        <br />
+        {eventInfo.event.extendedProps.hijriDate !== 'Pre-Hijri' && (
+          <>
+            Hijri: {eventInfo.event.extendedProps.hijriDate}
+            <br />
+          </>
+        )}
         Gregorian: {eventInfo.event.extendedProps.gregorianDate}
       </>
     )
@@ -146,7 +161,7 @@ const LunisolarHijriCalendar = () => {
         headerToolbar={{
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,dayGridWeek,dayGridDay'
+          right: 'dayGridYear,dayGridMonth,dayGridWeek,dayGridDay'
         }}
         events={events}
         eventContent={renderEventContent}
